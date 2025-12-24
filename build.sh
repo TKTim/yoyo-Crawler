97e92428cd125300de4e0e5303b215aa06bdaef1
tim-lee <tim_lee@trendmicro.com>
2025-12-24 13:45:21 +0800

    Debug: add verbose migration output to build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,17 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+echo "=== Running migrations ==="
+python manage.py showmigrations
+python manage.py migrate --verbosity 2
+
+echo "=== Checking tables ==="
+python -c "
+import sqlite3
+conn = sqlite3.connect('db.sqlite3')
+cursor = conn.cursor()
+cursor.execute(\"SELECT name FROM sqlite_master WHERE type='table';\")
+print('Tables:', [t[0] for t in cursor.fetchall()])
+conn.close()
+"
